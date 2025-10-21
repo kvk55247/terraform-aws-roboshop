2917ad0e921bb7f5ba2c73ca4a4ce98c5cce9e92
kvk55247 <kurapothulavamsi@gmail.com>
2025-10-21 12:38:42 +0530

    terraform

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "project" {
+    type = string
+    default = "roboshop"
+}
+
+variable "environment" {
+    type = string
+    default = "dev"
+}
+
+
+
+variable "zone_id" {
+    default = "Z02592383JVQTDY6U9ADB"
+}
+
+
+
+variable "zone_name" {
+    default = "daws84s.info"
+  
+}
+
+variable "component" {
+
+}
+
+variable "rule_priority" {
+    
+}
